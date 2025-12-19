577174e1c808d58536a590dd61c8135dd6873487
hitesh64 <hitesh64@gmail.com>
2025-12-19 11:34:04 +0530

    Fix backend crash: Add missing security deps and update API URL

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const API_URL = window.location.hostname === 'localhost' 
+const API_URL = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
     ? 'http://localhost:5000/api' 
     : '/api';
 
